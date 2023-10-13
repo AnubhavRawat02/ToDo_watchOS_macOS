b735c1c4587234130df115c34b439777e43ac250
Anubhav Rawat <anubhavrawat848@gmail.com>
2023-10-13 14:08:04 +0530

    Initial Commit

---
+++ ToDo_watchOS_macOS/ToDo_watchOS_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_watchOS_macOSApp.swift
+//  ToDo_watchOS_macOS
+//
+//  Created by Anubhav Rawat on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_watchOS_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
